0a6f59e04d64a5db88a3b57f6e859bd253498735
fatur2 <faturachman.ahmad@unida.ac.id>
2022-01-22 10:58:58 +0700

    Jawaban UAS ALPRO

---
+++ PatternFullPyramidOfStars.java
@@ -0,0 +1,16 @@
+
+public class PatternFullPyramidOfStars {
+	public static void main(String[] args) {
+		int a, b,c;
+		for (a=1;a <=5 ; a++) {
+			for (b=5; b>a; b--) {
+				System.out.print(" ");
+			}
+			for (c=1;c<(2*a) ; c++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
